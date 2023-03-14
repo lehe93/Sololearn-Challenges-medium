5c84c306b961419bc1f783a00d9f8a22cc330902
Leo <126462900+lehe93@users.noreply.github.com>
2023-03-14 15:48:44 +0100

    Update thatsodd_un.py

--- thatsodd_un.py
+++ thatsodd_un.py
@@ -1,3 +1,17 @@
-# Input sind mehrere Zahlen nacheinander. Alle geraden Zahlen sollen addiert werden
+# Input sind mehrere Zahlen nacheinander. Alle geraden Zahlen sollen addiert werden.
 
-nums = input().split("")
+lenlist = int(input())
+
+count = 0
+list = []
+summe = 0
+
+while count < lenlist:
+    list.append(int(input()))
+    count += 1
+
+for i in list:
+    if i % 2 == 0:
+        summe += i
+
+print(summe)
